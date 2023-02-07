5e007095f1525ba994ef5a192492c533a7dc330f
You-Wen Luo <youwenl1@uci.edu>
2023-02-07 00:59:04 -0800

    Add /chat junit test

--- src/test/java/org/example/commands/CommandDataTest.java
+++ src/test/java/org/example/commands/CommandDataTest.java
@@ -1,5 +1,6 @@
 package org.example.commands;
 
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
@@ -32,6 +33,28 @@ public class CommandDataTest {
         Assertions.assertTrue(option.getBoolean("required"));
         Assertions.assertEquals("message", option.getString("name"));
         Assertions.assertEquals("The message you want the bot say", option.getString("description"));
+
+    }
+
+    @Test
+    public void testSlashCommandWith1RequiredOption1OptionalOption(){
+        CommandData command = new CommandDataImpl("chat", "Make a bot chat in a channel")
+                .addOptions(new OptionData(OptionType.STRING, "content","The content you want a bot say",  true))
+                .addOptions(new OptionData(OptionType.CHANNEL, "channel", "The channel you want to sent this content in", false));
+        DataObject data = command.toData();
+        Assertions.assertEquals("chat", data.getString("name"));
+        Assertions.assertEquals("Make a bot chat in a channel", data.getString("description"));
+
+        DataArray options = data.getArray("options");
+        DataObject option = options.getObject(0);
+        Assertions.assertTrue(option.getBoolean("required"));
+        Assertions.assertEquals("content", option.getString("name"));
+        Assertions.assertEquals("The content you want a bot say", option.getString("description"));
+
+        option = options.getObject(1);
+        Assertions.assertFalse(option.getBoolean("required"));
+        Assertions.assertEquals("channel", option.getString("name"));
+        Assertions.assertEquals("The channel you want to sent this content in", option.getString("description"));
 
     }
 
